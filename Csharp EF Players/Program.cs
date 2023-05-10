970b1166223dd1dc194c8cf2f58c3916b138796b
emamingio22 <emawankenoby02@gmail.com>
2023-05-10 15:27:26 +0200

    connessione database

--- Csharp EF Players/Program.cs
+++ Csharp EF Players/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using Csharp_EF_Players;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+
+string stringaDiConnessione = "Data Source=localhost;" +
+    "Initial Catalog=EfPlayers;Integrated Security=True";
+
